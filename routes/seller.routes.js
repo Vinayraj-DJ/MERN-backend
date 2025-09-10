d4c6d61df036945e80c376b3f3fa45aa96108e63
Vinayraj-DJ <vinayrajdhondijugge@gmail.com>
2025-09-10 23:16:16 +0530

    nine commit

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -1,14 +1,32 @@
+// import express from "express";
+// import {
+//   checkAuth,
+//   sellerLogin,
+//   sellerLogout,
+// } from "../controller/seller.controller.js";
+// import { authSeller } from "../middlewares/authSeller.js";
+// const router = express.Router();
+
+// router.post("/login", sellerLogin);
+// router.get("/is-auth", authSeller, checkAuth);
+// router.get("/logout", authSeller, sellerLogout);
+
+// export default router;
+
+
 import express from "express";
-import {
-  checkAuth,
-  sellerLogin,
-  sellerLogout,
-} from "../controller/seller.controller.js";
+import { checkAuth, sellerLogin, sellerLogout } from "../controller/seller.controller.js";
 import { authSeller } from "../middlewares/authSeller.js";
+
 const router = express.Router();
 
+// Login
 router.post("/login", sellerLogin);
+
+// Check auth
 router.get("/is-auth", authSeller, checkAuth);
-router.get("/logout", authSeller, sellerLogout);
+
+// Logout (no auth middleware)
+router.post("/logout", sellerLogout);
 
 export default router;
