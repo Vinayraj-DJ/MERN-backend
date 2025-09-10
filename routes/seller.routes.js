cb3478d4234158eabc0c59a6b31760d0a3c5c3ed
Vinayraj-DJ <vinayrajdhondijugge@gmail.com>
2025-09-10 23:59:56 +0530

    thirteen commit

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -16,7 +16,7 @@
 
 import express from "express";
 import { checkAuth, sellerLogin, sellerLogout } from "../controller/seller.controller.js";
-import { authSeller } from "../middlewares/authSeller.js";
+// import { authSeller } from "../middlewares/authSeller.js";
 
 const router = express.Router();
 
@@ -24,7 +24,7 @@ const router = express.Router();
 router.post("/login", sellerLogin);
 
 // Check auth
-router.get("/is-auth", authSeller, checkAuth);
+router.get("/is-auth", checkAuth);
 
 // Logout (no auth middleware)
 router.post("/logout", sellerLogout);
