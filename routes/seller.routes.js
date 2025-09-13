b5e2c4a4a94218f1a06ef8935256e60701c525b8
Vinayraj-DJ <vinayrajdhondijugge@gmail.com>
2025-09-13 18:37:34 +0530

    thirtysix commit

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -45,7 +45,7 @@
 
 import express from "express";
 import { authSeller } from "../middlewares/authSeller.js";
-import { addProduct, listProducts, sellerLogin, checkAuth, sellerLogout } from "../controllers/seller.controller.js";
+import { addProduct, listProducts, sellerLogin, checkAuth, sellerLogout } from "../controller/seller.controller.js";
 
 const router = express.Router();
 
